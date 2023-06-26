889ab5976de4faed158234963bccca180274a221
De Jong Yeong <dejong.re2@gmail.com>
2023-06-26 23:33:11 +0100

    add enterprise ireland logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 import Questions from '@/components/Questions';
 import Score from '@/components/Score';
+import Image from 'next/image';
 
 export default function Home() {
   const [mounted, setMounted] = useState(false); // handle flash of unstyled content
@@ -13,16 +14,26 @@ export default function Home() {
   useEffect(() => setMounted(true), []);
   const design = !mounted
     ? 'invisible'
-    : 'visible flex flex-col min-h-screen items-center justify-center bg-gradient-to-b from-sky-100 to-sky-700';
+    : 'visible bg-gradient-to-b from-sky-100 to-sky-700';
 
   return (
     <main className={design}>
-      <div className="max-[480px]:m-5 max-[768px]:w-11/12 max-[1024px]:w-4/5 min-[1025px]:w-2/4">
-        {showScore ? (
-          <Score score={score} />
-        ) : (
-          <Questions setScore={setScore} setShowScore={setShowScore} />
-        )}
+      <div className=" h-screen flex flex-col items-center justify-center ">
+        <div className="max-[480px]:m-5 max-[768px]:w-11/12 max-[1024px]:w-4/5 min-[1025px]:w-2/4 ">
+          <Image
+            src="/ei-logo.png"
+            alt="Enterprise Ireland Logo"
+            width={250}
+            height={250}
+            className="mb-16"
+          />
+
+          {showScore ? (
+            <Score score={score} />
+          ) : (
+            <Questions setScore={setScore} setShowScore={setShowScore} />
+          )}
+        </div>
       </div>
     </main>
   );
